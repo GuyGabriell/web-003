95eed8808e30b50c7a19ffb8d300e8f8fa454ef2
GuyGabriell <jonahgabriel85@yahoo.com>
2024-06-25 11:16:28 +0100

    php-tutorial-laracasts

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+require 'functions.php';
+
+
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+
+
+if ($uri === '/') {
+
+  require 'controllers/index.php';
+
+}else if ($uri === '/about') {
+
+  require 'controllers/about.php';
+}else if ($uri === '/contact') {
+
+  require 'controllers/contact.php';
+}else if ($uri === '/testing') {
+
+  require 'controllers/testing.php';
+}
+
+
